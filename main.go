09d952af3aa9a37542389c445da355fdb61f67c3
dimitrijjedich <dimitrij@startup-werk.de>
2024-10-27 11:13:49 +0100

    implement newtons method for square root calculation

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 0; i < 10; i++ {
+		z = z - ((z*z - x) / (2 * z))
+	}
+	return z
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
