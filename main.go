a13b39eb18144a1dd2a56b9bd4a1e1cc293abe51
dimitrijjedich <dimitrij@startup-werk.de>
2024-10-28 21:29:55 +0100

    implement Pic function to use in pic.Show

--- main.go
+++ main.go
@@ -12,6 +12,19 @@ func sqrt(x float64) float64 {
 	return z
 }
 
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for y := range result {
+		result[y] = make([]uint8, dx)
+		for x := range result[y] {
+			//result[y][x] = uint8((x + y) / 2)
+			//result[y][x] = uint8(x * y)
+			result[y][x] = uint8(x ^ y)
+		}
+	}
+	return result
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
