86342a81d2d2b89afd3265ad267b590f936dcc02
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-07 18:48:13 +0100

    add conditional logic
    this prevents the programm output from generating wrong gramatic when the missing years have to be singular

--- main.go
+++ main.go
@@ -13,7 +13,14 @@ func main() {
 	fmt.Scan(&age)
 	fmt.Printf("Hello, %s! You are %d years old!\n", name, age)
 	if age < 18 {
-		fmt.Printf("You are underage and it takes %d years until you are 18", 18-age)
+		var missing int = 18 - age
+		var years string
+		if missing == 1 {
+			years = "year"
+		} else {
+			years = "years"
+		}
+		fmt.Printf("You are underage and it takes %d %s until you are 18", missing, years)
 	} else {
 		fmt.Printf("You are not underage!")
 	}
