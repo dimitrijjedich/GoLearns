46932d03fae4124b00ac6028381c833360f139ee
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-07 19:04:32 +0100

    add structu for api data

--- bitcoin.go
+++ bitcoin.go
@@ -5,6 +5,39 @@ import (
 	"net/http"
 )
 
+type ApiData struct {
+	Time struct {
+		Updated    string    `json:"updated"`
+		UpdatedISO time.Time `json:"updatedISO"`
+		Updateduk  string    `json:"updateduk"`
+	} `json:"time"`
+	Disclaimer string `json:"disclaimer"`
+	ChartName  string `json:"chartName"`
+	Bpi        struct {
+		USD struct {
+			Code        string  `json:"code"`
+			Symbol      string  `json:"symbol"`
+			Rate        string  `json:"rate"`
+			Description string  `json:"description"`
+			RateFloat   float64 `json:"rate_float"`
+		} `json:"USD"`
+		GBP struct {
+			Code        string  `json:"code"`
+			Symbol      string  `json:"symbol"`
+			Rate        string  `json:"rate"`
+			Description string  `json:"description"`
+			RateFloat   float64 `json:"rate_float"`
+		} `json:"GBP"`
+		EUR struct {
+			Code        string  `json:"code"`
+			Symbol      string  `json:"symbol"`
+			Rate        string  `json:"rate"`
+			Description string  `json:"description"`
+			RateFloat   float64 `json:"rate_float"`
+		} `json:"EUR"`
+	} `json:"bpi"`
+}
+
 func main() {
 	var apiUrl string = "https://api.coindesk.com/v1/bpi/currentprice.json"
 	var client *http.Client = &http.Client{}
