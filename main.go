8307719123ce61d89d07224876d962f3e6e844d0
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-07 18:20:45 +0100

    refactor initialized values to user input

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 )
 
 func main() {
-	var name string = "Dimitrij"
-	var age int = 30
+	var name string
+	var age int
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Print("Enter your age: ")
+	fmt.Scan(&age)
 	fmt.Printf("Hello, %s! You are %d years old!", name, age)
 }
