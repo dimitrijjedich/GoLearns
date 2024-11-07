e8a8debb33d1d698764b02b7071d499ee11ddb8b
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-07 18:05:28 +0100

    cleanup project

--- go.mod
+++
@@ -1,5 +0,0 @@
-module GoLerns
-
-go 1.22
-
-require golang.org/x/tour v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"golang.org/x/tour/pic"
-)
-
-func sqrt(x float64) float64 {
-	z := float64(1)
-	for i := 0; i < 10; i++ {
-		z = z - ((z*z - x) / (2 * z))
-	}
-	return z
-}
-
-func Pic(dx, dy int) [][]uint8 {
-	result := make([][]uint8, dy)
-	for y := range result {
-		result[y] = make([]uint8, dx)
-		for x := range result[y] {
-			//result[y][x] = uint8((x + y) / 2)
-			//result[y][x] = uint8(x * y)
-			result[y][x] = uint8(x ^ y)
-		}
-	}
-	return result
-}
-
 func main() {
-	fmt.Println("Hello World")
-	pic.Show(Pic)
 }
