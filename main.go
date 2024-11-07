bc09ea6714a4768c57f09b35d5b0d6794ea551c1
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-07 18:26:33 +0100

    add linebreak for formatting

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Scan(&name)
 	fmt.Print("Enter your age: ")
 	fmt.Scan(&age)
-	fmt.Printf("Hello, %s! You are %d years old!", name, age)
+	fmt.Printf("Hello, %s! You are %d years old!\n", name, age)
 	if age < 18 {
 		fmt.Printf("You are underage and it takes %d years until you are 18", 18-age)
 	} else {
